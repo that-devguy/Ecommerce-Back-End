bef002b071fbc2c819fe2f61f5c4f85a32c466c3
that devguy <zkmutch@gmail.com>
2023-04-23 16:57:20 -0400

    added assosciations between models to index.js

--- models/index.js
+++ models/index.js
@@ -1,16 +1,20 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongToMany(Tag, { through: ProductTag });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, { through: ProductTag });
 
 module.exports = {
   Product,
